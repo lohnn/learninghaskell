2e5cb3102af08cbc86a9091da27d907c93597407
Johannes Löhnn <jlohnn@gmail.com>
2018-10-26 09:58:22 +0200

    Problem 3 added

--- problem1-10.hs
+++ problem1-10.hs
@@ -5,3 +5,7 @@ myLast (_:ab) = myLast ab
 myButLast::[a]->a
 myButLast [x, _] = x
 myButLast (_:xs) = myButLast xs
+
+elementAt::[a]->Int->a
+elementAt (x: _) 1 = x
+elementAt (_:xs) position = elementAt xs (position - 1)
